af25cbc7ff5b6c83b86247e0c752ff5178291bdc
chengenius <chenmastang@163.com>
2019-01-25 15:45:02 +0800

    first version commit

---
+++ src/Test.php
@@ -0,0 +1,20 @@
+<?php
+namespace Chengenius\Test;
+
+class Test
+{
+    public function __construct()
+    {
+    }
+    
+    /**
+     * this is just a demo action.
+     *
+     * @author chenmastang
+     * @return string
+     */
+    public function show()
+    {
+        return 'You are my super star, you know ?';
+    }
+}
